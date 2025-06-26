8a775bea62a7c8efd79ef9c2bcc8b98f5f5fb664
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-06-26 11:26:17 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My ToDo list</h1>
+      <ul>
+        <li>Buy groceries</li>
+        <li>Walk the dog</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
